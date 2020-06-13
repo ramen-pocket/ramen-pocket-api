9684384528f09c764b5ec6867f7134c6240c3917
virtualprism <alan159753@gmail.com>
2020-06-14 03:13:52 +0800

    Add readOneById to StoreRepository

--- src/repositories/store/store-repository.ts
+++ src/repositories/store/store-repository.ts
@@ -2,6 +2,7 @@ import { StoreEntity, NewStoreEntity } from '../../entities/store-entity';
 
 export interface StoreRepository {
   checkIdExistence(id: number): Promise<boolean>;
+  readOneById(id: number): Promise<StoreEntity>;
   readManyByIds(ids: number[]): Promise<StoreEntity[]>;
   readMany(limit: number, skip: number): Promise<StoreEntity[]>;
   createOne(newStore: NewStoreEntity): Promise<number>;
