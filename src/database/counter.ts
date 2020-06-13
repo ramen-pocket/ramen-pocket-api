f5100b09145835129cfb9678ddaa22e3edf82d89
virtualprism <alan159753@gmail.com>
2020-06-13 09:37:23 +0800

    Rename counter-query-result to counter

--- src/database/counter.ts
+++ src/database/counter.ts
@@ -2,6 +2,6 @@
  * A data transfer object for a commonly used query of counting records.
  * For example: `SELECT COUNT(*) AS count FROM users WHERE ...`
  */
-export class CounterQueryResult {
-  public counter: number;
+export class Counter {
+  public count: number;
 }
