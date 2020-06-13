6512ef9182f446a8381d0f583770d65f5b246e42
virtualprism <alan159753@gmail.com>
2020-06-14 03:35:59 +0800

    Create CommentUsecase and its implementation CommentService

---
+++ src/services/comment/comment-service.ts
@@ -0,0 +1,30 @@
+import { CommentUsecase } from './comment-usecase';
+import { CommentRepository } from '../../repositories/comment/comment-repository';
+import { NewCommentEntity } from '../../entities/new-comment-entity';
+import { UpdatedCommentEntity } from '../../entities/updated-comment-entity';
+import { UserProfileCommentGroupEntity } from '../../entities/user-profile-comment-group-entity';
+import { StoreCommentGroupEntity } from '../../entities/store-comment-group-entity';
+
+export class CommentService implements CommentUsecase {
+  public constructor(private readonly commentRepository: CommentRepository) {}
+
+  async readManyGroupedByUserByStoreId(storeId: number): Promise<UserProfileCommentGroupEntity[]> {
+    return this.commentRepository.readManyGroupedByUserByStoreId(storeId);
+  }
+
+  async readManyGroupedByStoreByUserId(userId: string): Promise<StoreCommentGroupEntity[]> {
+    return this.commentRepository.readManyGroupedByStoreByUserId(userId);
+  }
+
+  async createOne(newComment: NewCommentEntity): Promise<number> {
+    return this.commentRepository.createOne(newComment);
+  }
+
+  async updateOne(updatedComment: UpdatedCommentEntity): Promise<void> {
+    return this.commentRepository.updateOne(updatedComment);
+  }
+
+  async deleteOne(id: number, userId: string, storeId: number): Promise<void> {
+    return this.commentRepository.deleteOne(id, userId, storeId);
+  }
+}
---
+++ src/services/comment/comment-usecase.ts
@@ -0,0 +1,12 @@
+import { StoreCommentGroupEntity } from '../../entities/store-comment-group-entity';
+import { UserProfileCommentGroupEntity } from '../../entities/user-profile-comment-group-entity';
+import { NewCommentEntity } from '../../entities/new-comment-entity';
+import { UpdatedCommentEntity } from '../../entities/updated-comment-entity';
+
+export interface CommentUsecase {
+  readManyGroupedByUserByStoreId(storeId: number): Promise<UserProfileCommentGroupEntity[]>;
+  readManyGroupedByStoreByUserId(userId: string): Promise<StoreCommentGroupEntity[]>;
+  createOne(newComment: NewCommentEntity): Promise<number>;
+  updateOne(updatedComment: UpdatedCommentEntity): Promise<void>;
+  deleteOne(id: number, userId: string, storeId: number): Promise<void>;
+}
