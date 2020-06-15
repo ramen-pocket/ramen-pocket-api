3a45fc6e4f147445452ed1a7bb129fd8eacabab5
virtualprism <alan159753@gmail.com>
2020-06-15 08:49:29 +0800

    Fix the response body type of GetUserCollectionHandler

--- src/controllers/user/get-user-collections-handler.ts
+++ src/controllers/user/get-user-collections-handler.ts
@@ -3,6 +3,7 @@ import { RequestHandler } from '../interfaces/request-handler';
 import { CollectionUsecase } from '../../services/collection/collection-usecase';
 import { ExtendedAPIGatewayProxyEvent } from '../interfaces/extended-api-gateway-proxy-event';
 import { StoreDto } from '../dtos/store-dto';
+import { GetUserCollectionsDto } from '../dtos/get-user-collections-dto';
 import { ResponseBuilder, HttpCode } from '../utils/response-builder';
 
 export class GetUserCollectionsHandler implements RequestHandler {
@@ -11,13 +12,14 @@ export class GetUserCollectionsHandler implements RequestHandler {
   public async handle(event: ExtendedAPIGatewayProxyEvent): Promise<APIGatewayProxyResult> {
     const { userId } = event.requestContext.authorizer;
     const stores = await this.collectionUsecase.readStoresFromUserCollection(userId);
-    const collectiveStores: StoreDto[] = stores.map((store) =>
+    const storeDtos: StoreDto[] = stores.map((store) =>
       StoreDto.transformFromStoreEntity(store, true),
     );
 
+    const responseBody: GetUserCollectionsDto = { stores: storeDtos };
     return ResponseBuilder.setup()
       .setStatusCode(HttpCode.OK)
-      .setBody(collectiveStores)
+      .setBody(responseBody)
       .build();
   }
 }
